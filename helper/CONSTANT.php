2e16851a645011ed68babde5639c2df5c3fa6429
jof12 <72898201+jof12@users.noreply.github.com>
2021-06-05 14:08:43 +0800

    Add files via upload

---
+++ helper/CONSTANT.php
@@ -0,0 +1,12 @@
+<?php
+
+// connection
+define("HOST", "db-bookstore.c4qhnxhwjra7.us-east-1.rds.amazonaws.com");
+define("UNAME", "tutorial_user");
+define("PASS", "Kamangta");
+define("DB", "dbtoko_buku");
+
+// tables
+// define("TB_BARANG", "tb_barang");
+
+?>
